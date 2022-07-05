2a9660900524ed5d45ab27222ed600f74fe4757e
Monika Łyżwa <lyzwa.monika@gmail.com>
2022-07-05 09:54:20 +0200

    Complete the Answer component template
    Detailed description:
    - create a variable with converting grams into glasses, spoons and
    teaspoons using props
    - display the result

--- src/Answer.js
+++ src/Answer.js
@@ -1,24 +1,17 @@
 import styled from "styled-components";
 
-const Answer = () => {
+const Answer = (props) => {
+  const score = (props.mass / props.volume / props.density).toFixed(2);
   return (
     <Size>
-      <span>🥛</span>
-      <span>🥄</span>
-      <span>🥄</span>
+      {props.icon} {props.mass <= 0 ? "" : score}
     </Size>
   );
 };
 
 const Size = styled.div`
-  margin-top: 30px;
-  display: flex;
-  flex-direction: column;
-
-  & > span {
-    margin: 20px 20px 20px 10px;
-    font-size: 20px;
-  }
+  margin: 20px 20px 20px 10px;
+  font-size: 20px;
 `;
 
 export default Answer;
