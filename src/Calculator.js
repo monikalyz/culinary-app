b4a47568c4f5ee451e68c51b9e97e4ac9779c9ae
Monika Łyżwa <lyzwa.monika@gmail.com>
2022-07-05 09:53:01 +0200

    Complete the Calculator component template
    Detailed description:
    - define state variables and do destructuring for them
    - defining static variables - a list of objects with product data, the density of each product
    - handle events for select, input
    - create a function that selects the density of a specific product
    - make a copy of the object sizes list -  and provide all the necessary
    data

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,13 +3,76 @@ import styled from "styled-components";
 import Answer from "./Answer";
 
 class Calculator extends Component {
-  state = {};
+  state = {
+    product: "flour",
+    mass: "",
+  };
+
+  static defaultProps = {
+    sizes: [
+      {
+        id: 0,
+        name: "glass",
+        volume: 250,
+        icon: "🥛",
+      },
+      {
+        id: 1,
+        name: "spoon",
+        volume: 15,
+        icon: "🥄",
+      },
+      {
+        id: 2,
+        name: "teaspoon",
+        volume: 5,
+        icon: "🥄",
+      },
+    ],
+
+    densities: {
+      flour: 0.69,
+      sugar: 0.88,
+      honey: 1.44,
+      oil: 0.92,
+      milk: 1,
+    },
+  };
+
+  handleSelect = (e) => {
+    this.setState({
+      product: e.target.value,
+      mass: "",
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      mass: e.target.value,
+    });
+  };
+
+  selectDensity = (select) => {
+    const density = this.props.densities[select];
+    return density;
+  };
+
   render() {
+    const { product, mass } = this.state;
+    const calculators = this.props.sizes.map((size) => (
+      <Answer
+        key={size.id}
+        volume={size.volume}
+        icon={size.icon}
+        mass={mass}
+        density={this.selectDensity(product)}
+      />
+    ));
     return (
       <Form>
         <label>
           Wybierz produkt:
-          <select>
+          <select value={product} onChange={this.handleSelect}>
             <option value="flour">mąka</option>
             <option value="sugar">cukier</option>
             <option value="honey">miód</option>
@@ -21,9 +84,9 @@ class Calculator extends Component {
         <br />
         <label>
           Podaj ilość w gramach:
-          <input type="number" />
+          <input type="number" value={mass} onChange={this.handleChange} />
         </label>
-        <Answer></Answer>
+        <Answers>{calculators}</Answers>
       </Form>
     );
   }
@@ -31,6 +94,12 @@ class Calculator extends Component {
 
 const Form = styled.div`
   transform: translatex(calc(50% - 110px));
+`;
+
+const Answers = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
 `;
 
 export default Calculator;
